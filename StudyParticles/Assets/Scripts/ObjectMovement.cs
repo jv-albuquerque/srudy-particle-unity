f2c636eb4c8e3bd5b505fab270a031b0f0670a3e
João Albuquerque <jv.albuquerque@live.com>
2019-07-08 18:40:52 -0300

    first try to do a good trail

---
+++ StudyParticles/Assets/Scripts/ObjectMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    public Transform[] targets;
+    public bool updown = false;
+    public bool move = false;
+
+    private bool up = true;
+    private Vector3 vUpDown;
+
+    private int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(move)
+            Move();
+        if(updown)
+            UpDown();
+    }
+
+    private void Move()
+    {
+        if (Vector3.Distance(targets[index].position, transform.position) <= 0.2f)
+        {
+            index++;
+
+            if (index >= targets.Length)
+                index = 0;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targets[index].position, .1f);
+        }
+    }
+
+    private void UpDown()
+    {
+
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time*10)/6 + 1, transform.position.z);
+    }
+}
